c3e641a69041f411add59247d459bc1d55faf563
Ramanarayana Narmala <3530406+suvera@users.noreply.github.com>
2022-11-02 11:23:22 +0530

    Update unblocked_script_50.js

--- unblocked_script_50.js
+++ unblocked_script_50.js
@@ -1,1 +1,6 @@
 console.log("XXXXXX : Unblocked Script 50 loaded");
+
+if (typeof setCookie === "function") {
+    setCookie("unblocked_script_cookie_01", "Hello,World!", 2)
+    setCookie("unblocked_script_cookie_02", "xxxxxxxx", 2)
+}
