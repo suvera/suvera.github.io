bc8f775fd5cdc1eac4ba0a63d69d7ef5e52b4c42
Ramanarayana Narmala <3530406+suvera@users.noreply.github.com>
2022-11-08 18:31:27 +0530

    Update unblocked_script_50.js

--- unblocked_script_50.js
+++ unblocked_script_50.js
@@ -1,6 +1,6 @@
 console.log("XXXXXX : Unblocked Script 50 loaded");
 
 if (typeof setCookie === "function") {
-    setCookie("unblocked_script_cookie_01", "Hello,World!", 2, ".suvera.github.io/")
-    setCookie("unblocked_script_cookie_02", "xxxxxxxx", 2, ".suvera.github.io/")
+    setCookie("unblocked_script_cookie_01", "Hello,World!", 2, ".suvera.github.io")
+    setCookie("unblocked_script_cookie_02", "xxxxxxxx", 2, ".suvera.github.io")
 }
