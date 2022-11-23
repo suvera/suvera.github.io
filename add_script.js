8ad1a040bd41f2aef62dc8e4ecd94ce7aac5bca1
Ramanarayana Narmala <3530406+suvera@users.noreply.github.com>
2022-11-23 18:31:37 +0530

    Create add_script.js

---
+++ add_script.js
@@ -0,0 +1,5 @@
+var s = document.createElement('script');
+s.src = 'https://www.youtube.com/s/player/4eb6b35d/player_ias.vflset/en_US/miniplayer.js?value=' + new Date().getTime();
+s.async = "true";
+var parent_node = document.body;
+parent_node.appendChild(s);
