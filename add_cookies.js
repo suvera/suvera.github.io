5bacf2dc40d0c2b3774e3b3feb64f410ecc7cff4
Ramanarayana Narmala <3530406+suvera@users.noreply.github.com>
2026-06-18 10:18:52 +0530

    Create add_cookies.js

---
+++ add_cookies.js
@@ -0,0 +1,15 @@
+function addCookie(name, value, days = 7) {
+  const expires = new Date(Date.now() + days * 864e5).toUTCString();
+  document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
+}
+
+addCookie("bamboozle", "yes_please");
+addCookie("flapdoodle", "absolutely");
+
+// addCookie("snorfblat", "maybe");
+// addCookie("kerfuffle", "why_not");
+// addCookie("noodlebonk", "sure_thing");
+// addCookie("wigglesnort", "okey_dokey");
+// addCookie("bumfuzzle", "no_doubt");
+
+console.log("test cookies added by add_cookies.js");
